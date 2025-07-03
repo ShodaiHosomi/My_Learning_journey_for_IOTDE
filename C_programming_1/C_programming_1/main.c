d28effe8077101d066f3f5a54a09f27095accfcd
ShodaiHosomi <siguren1475@gmail.com>
2025-07-03 14:34:25 +0900

    Add Visual Studio C learning project from Windows11

---
+++ C_programming_1/C_programming_1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main() {
+	printf("%d + %d = %d\n", 5, 2, 5 + 2);
+	printf("%d - %d = %d\n", 5, 2, 5 - 2);
+	printf("%d * %d = %d\n", 5, 2, 5 * 2);
+	printf("%d / %d = %d the remain is %d \n", 5, 2, 5 / 2, 5 % 2);
+}
